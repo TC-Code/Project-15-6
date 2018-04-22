9ab7b38c71e24639dd4bffe6303649c5c0e8fba3
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-04-22 21:02:08 +0200

    Add 'reset' button

--- script-compiled.js
+++ script-compiled.js
@@ -95,3 +95,8 @@ var stopButton = document.getElementById("stop");
 stopButton.addEventListener("click", function () {
   return stopwatch.stop();
 });
+
+var resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", function () {
+  stopwatch.reset(), stopwatch.print();
+});
